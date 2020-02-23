050fe7cda0324920cc05bd6fcb3f06338734ea68
Joel Hobgood <jhobgoo@gmail.com>
2020-02-23 02:38:22 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,8 +14,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+     
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,31 +56,40 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages] = "C"
+  programmer_hash[:dennis_ritchie][:languages]
+  
 end
 
 def adding_matz
+
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{
-        :grace_hopper => {
+	programmer_hash = {   
+ 	        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+          :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
+          :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
      }
-
+     
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+    programmer_hash
     
 end
 
@@ -99,10 +109,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
-     
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
+    programmer_hash
 end
+
